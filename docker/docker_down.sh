5e1e91b242583eb355802b0f75b96a4a2241ce53
Kevin Cao <kevincao222@gmail.com>
2025-01-11 11:49:17 -0800

    Add env file to docker down commands

--- docker/docker_down.sh
+++ docker/docker_down.sh
@@ -1,11 +1,11 @@
 echo "Tearing down dev env container..."
-docker compose --file ./dev_env/docker-compose.yml down -v
+docker compose --file ./dev_env/docker-compose.yml  --env-file .env down -v
 
 echo "Tearing down Airflow containers..."
-docker compose --file ./airflow/docker-compose.yml down -v
+docker compose --file ./airflow/docker-compose.yml  --env-file .env down -v
 
 echo "Tearing down Airflow containers..."
-docker compose --file ./spark/docker-compose.yml down -v
+docker compose --file ./spark/docker-compose.yml  --env-file .env down -v
 
 echo "Pruning unused volumes..."
 docker system prune --volumes -f &
