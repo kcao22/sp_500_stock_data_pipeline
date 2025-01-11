8a82fee422e0add23b27b01b21d47723040fb432
Kevin Cao <kevincao222@gmail.com>
2025-01-11 10:33:52 -0800

    Fix docker compose build context pathing issues

--- docker/docker_up.sh
+++ docker/docker_up.sh
@@ -1,8 +1,8 @@
 echo "Building dev env container"
-docker compose --file ./dev_env/docker-compose.yml up
+docker compose --file ./dev_env/docker-compose.yml --env-file .env up
 
 echo "Building Airflow containers..."
-docker compose --file ./airflow/docker-compose.yml up
+docker compose --file ./airflow/docker-compose.yml --env-file .env up
 
 echo "Building Spark containers..."
-docker compose --file ./spark/docker-compose.yml up
+docker compose --file ./spark/docker-compose.yml --env-file .env up
