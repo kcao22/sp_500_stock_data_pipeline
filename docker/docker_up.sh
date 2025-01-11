44f56a12090642e23ee327831861c40bdf590945
Kevin Cao <kevincao222@gmail.com>
2025-01-11 11:18:41 -0800

    Add -d to docker compose up

--- docker/docker_up.sh
+++ docker/docker_up.sh
@@ -1,8 +1,8 @@
 echo "Building dev env container"
-docker compose --file ./dev_env/docker-compose.yml --env-file .env up
+docker compose --file ./dev_env/docker-compose.yml --env-file .env up -d
 
 echo "Building Airflow containers..."
-docker compose --file ./airflow/docker-compose.yml --env-file .env up
+docker compose --file ./airflow/docker-compose.yml --env-file .env up -d
 
 echo "Building Spark containers..."
-docker compose --file ./spark/docker-compose.yml --env-file .env up
+docker compose --file ./spark/docker-compose.yml --env-file .env up -d
