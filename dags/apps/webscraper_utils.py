986e4979ae6c0d1bc1ee0d0f3884463d660a7e54
kcao22 <kevincao222@gmail.com>
2025-02-09 11:38:18 -0800

    Update webscraper utils to handle text vs data value fields

--- dags/apps/webscraper_utils.py
+++ dags/apps/webscraper_utils.py
@@ -29,15 +29,29 @@ class BeautifulSoupScraper:
             )
             return BeautifulSoup(response.text, "html.parser")
         except Exception as e:
-            raise Exception(f"Failed to request page for {url} after 5 retries with exception:"
-                            f" {e}") from e
+            raise Exception(f"Failed to request page for {url} after 5 retries with exception:\n{e}") from e
 
-    def get_element_value(self, soup_object: BeautifulSoup, html_element: str, identifier_attribute:
-    str, identifier_attribute_value) -> str:
+    def get_element_data_value(self, soup_object: BeautifulSoup, html_element_tag: str, identifier_attribute: str, identifier_value: str) -> str:
         try:
-            return soup_object.find(html_element, {identifier_attribute:
-                                                       identifier_attribute_value}).get("value")
+            element = soup_object.find(html_element_tag, {identifier_attribute: identifier_value})
+            if not element:
+                return None
+            if element.has_attr("data-value"):
+                return element["data-value"].strip()
+            else:
+                return element.text.strip()
         except Exception as e:
-            raise Exception(f"Failed to find HTML element {html_element} with identifier "
-                            f"attribute {identifier_attribute} and value "
-                            f"{identifier_attribute_value}. \nException: {e}")
+            raise Exception(f"Get element failed for {html_element_tag}, identifier attribute {identifier_attribute}, and identifier_value {identifier_value} with exception: {e}")
+
+    def get_element_text_value(self, soup_object: BeautifulSoup, html_element_tag: str, text_class_name=None) -> str:
+        try:
+            label_span = soup_object.find("span", class_="label", string=lambda s: s and html_element_label in s)
+            if not label_span or not label_span.find_next_sibling():
+                return None
+            value_span = label_span.find_next_sibling("span", class_=text_class_name)
+            if value_span:
+                return value_span.text.strip()
+            else:
+                return None
+        except Exception as e:
+            raise Exception(f"Error occurred when extracting text value for {html_element_label} and text class name {text_class_name} with exception: {e}")
