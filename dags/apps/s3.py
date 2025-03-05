3bf20d1ce186449b378170c648788a5764595179
kcao22 <kevincao222@gmail.com>
2025-03-04 21:30:54 -0800

    Add s3 functions

--- dags/apps/s3.py
+++ dags/apps/s3.py
@@ -13,11 +13,20 @@ def _choose_s3_bucket(is_test: bool, bucket: str):
                 return "archive"
 
 
-def put_object(is_test: bool, target_bucket: str, key: str, body: str, **kwargs) -> None:
+def _create_client():
+    client = boto3.client(
+        "s3",
+        aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
+    )
+    return client
+
+
+def put_object(is_test: bool, bucket: str, key: str, body: str, **kwargs) -> None:
     """
     Puts object to target s3 bucket. If local, writes to a mimic bucket in /opt/airflow/files/.
     @param is_test: If True, then local. Else AWS prod environment.
-    @param target_bucket: If local, the folder of interest. Else S3 target bucket name.
+    @param bucket: If local, the folder of interest. Else S3 target bucket name.
     @param key: Full prefix and name of file.
     @param body: Content of file.
     @param kwargs: Keyword arguments.
@@ -34,18 +43,87 @@ def put_object(is_test: bool, target_bucket: str, key: str, body: str, **kwargs)
             with open(file_path, "wb") as f:
                 f.write(body)
         except Exception as e:
-            raise Exception(f"Failed to put object to target bucket {target_bucket} with object path {key}. Exception: {e}")
+            raise Exception(f"Failed to put object to target bucket {bucket} with object path {key}. Exception: {e}")
     else:
         try:
-            client = boto3.client(
-                "s3",
-                aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
-                aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
-            )
+            client = _create_client()
             client.put_object(
-                Bucket=_choose_s3_bucket(is_test=is_test, bucket=target_bucket),
+                Bucket=_choose_s3_bucket(is_test=is_test, bucket=bucket),
                 Key=key,
-                Body=body
+                Body=body,
+                **kwargs
             )
         except Exception as e:
-            raise Exception(f"Failed to put object for {key} to bucket {target_bucket} with exception: {e}") from e
+            raise Exception(f"Failed to put object for {key} to bucket {bucket} with exception: {e}") from e
+
+
+def get_object(is_test: bool, bucket: str, key: str, **kwargs) -> dict:
+    """
+    Gets object from a bucket.
+    @param is_test: If True, then local. Else AWS prod environment.
+    @param bucket: Bucket name.
+    @param key: Full path of the object.
+    @param kwargs: Keyword arguments.
+    @return: Object content.
+    """
+    if is_test:
+        try:
+            with open(f"/opt/airflow/files/{_choose_s3_bucket(is_test=is_test, bucket=bucket)}/{key}", "rb") as f:
+                return {"Body": f.read()}
+        except UnicodeDecodeError:
+            with open(f"/opt/airflow/files/{_choose_s3_bucket(is_test=is_test, bucket=bucket)}/{key}", "rb") as f:
+                return {"Body": f.read()}
+    else:
+        client = _create_client()
+        return client.put_object(
+            Bucket=_choose_s3_bucket(is_test=is_test, bucket=bucket),
+            Key=key,
+            **kwargs
+        )
+
+
+def copy_object(is_test: bool, source_bucket: str, source_key: str, target_bucket: str, target_key: str, **kwargs) -> None:
+    """
+    Copies object from a source bucket to target bucket.
+    @param is_test: If True, then local. Else AWS prod environment.
+    @param source_bucket: Source bucket name.
+    @param source_key: Full path of the source object.
+    @param target_bucket: Target bucket name.
+    @param target_key: Full path of target object.
+    @param kwargs: Keyword arguments.
+    @return: None.
+    """
+    if is_test:
+        body = get_object(
+            is_test=is_test, 
+            bucket=_choose_s3_bucket(is_test=is_test, bucket=source_bucket), 
+            key=source_key
+        )["Body"]
+        put_object(
+            is_test=is_test, 
+            bucket=_choose_s3_bucket(is_test=is_test, bucket=target_bucket), 
+            key=target_key, 
+            body=body
+        )
+    else:
+        try:
+            client = _create_client()
+            client.copy_object(
+                Bucket=_choose_s3_bucket(is_test=is_test, bucket=target_bucket),
+                Key=target_key,
+                CopySource={"Bucket": source_bucket, "Key": source_key},
+            )
+        except Exception as e:
+            raise Exception(f"Failed to copy object from source bucket {source_bucket} with object path {source_key} to target bucket {target_bucket} with object path {target_key}. Exception: {e}") from e
+
+
+def delete_object(is_test: bool, bucket: str, key: str, **kwargs) -> None:
+    if is_test:
+        os.remove(f"/opt/airflow/files/{key}")
+    else:
+        client = _create_client()
+        client.delete_object(
+            Bucket=_choose_s3_bucket(is_test=is_test, bucket=bucket),
+            Key=key,
+            **kwargs
+        )
