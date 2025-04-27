f0e4de40f2ef8038b1815ca1019a3c0a616ec6c8
Kevin Cao <kevincao222@gmail.com>
2025-04-26 18:55:49 -0700

    add prod tables

--- migrations/V2025_03_26.sql
+++ migrations/V2025_03_26.sql
@@ -1,9 +1,9 @@
--- Create raw table for daily data
-DROP SCHEMA IF EXISTS raw;
-CREATE SCHEMA IF NOT EXISTS raw;
+-- Create ingress table for daily data
+DROP SCHEMA IF EXISTS ingress;
+CREATE SCHEMA IF NOT EXISTS ingress;
 
-DROP TABLE IF EXISTS raw.companies_daily;
-CREATE TABLE IF NOT EXISTS raw.companies_daily (
+DROP TABLE IF EXISTS ingress.companies_daily;
+CREATE TABLE IF NOT EXISTS ingress.companies_daily (
     symbol VARCHAR(65535) NOT NULL,
     load_timestamp_utc VARCHAR(65535) NOT NULL,
     previous_close VARCHAR(65535),
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS raw.companies_daily (
     one_year_target_estimate VARCHAR(65535)
 );
 
-DROP TABLE IF EXISTS raw.companies_weekly;
-CREATE TABLE IF NOT EXISTS raw.companies_weekly (
+DROP TABLE IF EXISTS ingress.companies_weekly;
+CREATE TABLE IF NOT EXISTS ingress.companies_weekly (
     symbol VARCHAR(65535) NOT NULL,
     load_timestamp_utc VARCHAR(65535) NOT NULL,
     company_name VARCHAR(65535),
@@ -38,3 +38,44 @@ CREATE TABLE IF NOT EXISTS raw.companies_weekly (
     company_corporate_governance_score VARCHAR(65535)
 );
 
+-- Create ods table for daily data
+DROP SCHEMA IF EXISTS ods;
+CREATE SCHEMA IF NOT EXISTS ods;
+
+DROP TABLE IF EXISTS ods.companies_daily;
+CREATE TABLE IF NOT EXISTS ods.companies_daily (
+    symbol VARCHAR(16) NOT NULL,
+    load_timestamp_utc TIMESTAMP NOT NULL,
+    previous_close NUMERIC(9,2),
+    "open" NUMERIC(9,2),
+    bid VARCHAR(64),
+    ask VARCHAR(64),
+    day_range VARCHAR(64),
+    volume BIGINT),
+    avg_volume BIGINT,
+    intraday_market_cap VARCHAR(32),
+    beta NUMERIC(5,2),
+    pe_ratio NUMERIC(7,2),
+    eps NUMERIC(7,2),
+    earnings_date VARCHAR(128),
+    forward_dividend_and_yield VARCHAR(64),
+    ex_dividend_date VARCHAR(128),
+    one_year_target_estimate NUMERIC(9,2),
+    PRIMARY KEY (symbol, load_timestamp_utc)
+);
+
+DROP TABLE IF EXISTS ods.companies_weekly;
+CREATE TABLE IF NOT EXISTS ods.companies_weekly (
+    symbol VARCHAR(16) NOT NULL,
+    load_timestamp_utc TIMESTAMP NOT NULL,
+    company_name VARCHAR(256),
+    company_address VARCHAR(512),
+    company_phone_number VARCHAR(64),
+    company_website VARCHAR(512),
+    company_sector VARCHAR(128),
+    company_industry VARCHAR(128),
+    company_full_time_employees INTEGER,
+    company_description VARCHAR(65535),
+    company_corporate_governance_score VARCHAR(1024),
+    PRIMARY KEY (symbol, load_timestamp_utc)
+);
