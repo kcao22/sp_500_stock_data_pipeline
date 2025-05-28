f3e68ae7808f16fc63d41f16761c8e51b1647fb3
kcao22 <kevincao222@gmail.com>
2025-05-27 22:22:16 -0700

    Update schema names

--- migrations/V2025_03_26.sql
+++ migrations/V2025_03_26.sql
@@ -1,13 +1,13 @@
--- Create ingress table for daily data
-DROP SCHEMA IF EXISTS ingress;
-CREATE SCHEMA IF NOT EXISTS ingress;
+-- Create ingress_yahoo table for daily data
+DROP SCHEMA IF EXISTS ingress_yahoo;
+CREATE SCHEMA IF NOT EXISTS ingress_yahoo;
 
-DROP TABLE IF EXISTS ingress.companies_daily;
-CREATE TABLE IF NOT EXISTS ingress.companies_daily (
+DROP TABLE IF EXISTS ingress_yahoo.companies_daily;
+CREATE TABLE IF NOT EXISTS ingress_yahoo.companies_daily (
     symbol VARCHAR(65535) NOT NULL,
     load_timestamp_utc VARCHAR(65535) NOT NULL,
     previous_close VARCHAR(65535),
-    "open" VARCHAR(65535),
+    open_price VARCHAR(65535),
     bid VARCHAR(65535),
     ask VARCHAR(65535),
     day_range VARCHAR(65535),
@@ -23,8 +23,8 @@ CREATE TABLE IF NOT EXISTS ingress.companies_daily (
     one_year_target_estimate VARCHAR(65535)
 );
 
-DROP TABLE IF EXISTS ingress.companies_weekly;
-CREATE TABLE IF NOT EXISTS ingress.companies_weekly (
+DROP TABLE IF EXISTS ingress_yahoo.companies_weekly;
+CREATE TABLE IF NOT EXISTS ingress_yahoo.companies_weekly (
     symbol VARCHAR(65535) NOT NULL,
     load_timestamp_utc VARCHAR(65535) NOT NULL,
     company_name VARCHAR(65535),
@@ -38,16 +38,16 @@ CREATE TABLE IF NOT EXISTS ingress.companies_weekly (
     company_corporate_governance_score VARCHAR(65535)
 );
 
--- Create ods table for daily data
-DROP SCHEMA IF EXISTS ods;
-CREATE SCHEMA IF NOT EXISTS ods;
+-- Create ods_yahoo table for daily data
+DROP SCHEMA IF EXISTS ods_yahoo;
+CREATE SCHEMA IF NOT EXISTS ods_yahoo;
 
-DROP TABLE IF EXISTS ods.companies_daily;
-CREATE TABLE IF NOT EXISTS ods.companies_daily (
+DROP TABLE IF EXISTS ods_yahoo.companies_daily;
+CREATE TABLE IF NOT EXISTS ods_yahoo.companies_daily (
     symbol VARCHAR(16) NOT NULL,
     load_timestamp_utc TIMESTAMP NOT NULL,
     previous_close NUMERIC(9,2),
-    "open" NUMERIC(9,2),
+    open_price NUMERIC(9,2),
     bid VARCHAR(64),
     ask VARCHAR(64),
     day_range VARCHAR(64),
@@ -64,8 +64,8 @@ CREATE TABLE IF NOT EXISTS ods.companies_daily (
     PRIMARY KEY (symbol, load_timestamp_utc)
 );
 
-DROP TABLE IF EXISTS ods.companies_weekly;
-CREATE TABLE IF NOT EXISTS ods.companies_weekly (
+DROP TABLE IF EXISTS ods_yahoo.companies_weekly;
+CREATE TABLE IF NOT EXISTS ods_yahoo.companies_weekly (
     symbol VARCHAR(16) NOT NULL,
     load_timestamp_utc TIMESTAMP NOT NULL,
     company_name VARCHAR(256),
