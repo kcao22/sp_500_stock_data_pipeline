61dcabc94fc9744c5dad354a34a3c68a177057be
kcao22 <kevincao222@gmail.com>
2025-06-04 20:01:34 -0700

    Uncomment dbt docker compse up

--- docker/docker_up.sh
+++ docker/docker_up.sh
@@ -11,7 +11,7 @@ echo "Building localstack AWS containers..."
 docker compose --file ./aws/docker-compose.yml --env-file .env up -d
 
 echo "Building dbt containers..."
-# docker compose --file ./dbt/docker-compose.yml --env-file .env up -d
+docker compose --file ./dbt/docker-compose.yml --env-file .env up -d
 
 echo "Building Spark containers..."
 docker compose --file ./spark/docker-compose.yml --env-file .env up -d
