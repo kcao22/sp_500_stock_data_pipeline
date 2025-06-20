fc963e473e466e0c9fbddeeed16df9e7639c5748
Kevin Cao <kevincao222@gmail.com>
2025-06-20 12:41:57 -0700

    Remove consumer from utils

--- dags/apps/kafka_utils.py
+++ dags/apps/kafka_utils.py
@@ -3,7 +3,7 @@ import pendulum
 import random
 import time
 from uuid import uuid4
-from confluent_kafka import Producer, Consumer
+from confluent_kafka import Producer
 
 
 def create_producer(endpoint: str = "kafka:9092") -> Producer:
@@ -13,30 +13,6 @@ def create_producer(endpoint: str = "kafka:9092") -> Producer:
     return Producer({
         "bootstrap.servers": endpoint
     })
-
-def create_consumer(
-        endpoint: str = "kafka:9092",
-        consumer_group: str = "stock_transactions_consumer_group", offset_handling: str = "earliest",
-        topic: str = "stock_transactions"
-    ) -> Consumer:
-    """
-    Creates a Kafka consumer, connecting to a specified endpoint as a part of a specified consumer group and subscribes to a given topic.
-    :param endpoint: The Kafka broker endpoint to connect to.
-    :param consumer_group: The consumer group to which the consumer belongs.
-    :param offset_handling: How to handle offsets, e.g., "earliest" or "latest".
-    :param topic: The topic to subscribe to.
-    :return: Kafka consumer instance.
-    """
-    stock_transaction_consumer = Consumer(
-        {
-            "bootstrap.servers": endpoint,
-            "group.id": consumer_group,
-            "auto.offset.reset": offset_handling
-        }
-    )
-    stock_transaction_consumer.subscribe([topic])
-    return stock_transaction_consumer
-
 
 def produce_transaction():
     """
