ccabcd5aebfe9200cbb5a536d6a0c09c79cfe07b
Kevin Cao <kevincao222@gmail.com>
2025-06-23 11:27:14 -0700

    Move script execution to individual module

---
+++ dags/apps/streaming/transactions_producer.py
@@ -0,0 +1,13 @@
+import logging
+import time
+
+from utils import YahooFinanceTransactionsAvroProducer
+
+if __name__ == "__main__":
+    transaction_producer = YahooFinanceTransactionsAvroProducer()
+    try:
+        while True:
+            transaction_producer.produce_transaction()
+            time.sleep(5)
+    except KeyboardInterrupt:
+        logging.info(f"Transaction production terminated by user. Shutting down...")
